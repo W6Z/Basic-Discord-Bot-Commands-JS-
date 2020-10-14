be5d6eb803a4f672f4dafa2b17614222a912325a
Wud <68764672+W6Z@users.noreply.github.com>
2020-10-13 21:49:43 -0500

    Create Presence.js

---
+++ Presence.js
@@ -0,0 +1,10 @@
+client.on("ready", () =>{
+    console.log(`Status has worked!`);
+    client.user.setPresence({
+        status: "online",  // You can show online, idle... Do not disturb is dnd
+        game: {
+            name: "!help",  // The message shown
+            type: "PLAYING" // PLAYING, WATCHING, LISTENING, STREAMING,
+        }
+    });
+ });
