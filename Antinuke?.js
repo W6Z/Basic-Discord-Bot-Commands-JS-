f88e3581ed6dca514504310247af10e0fadab49c
ItsVops <55073114+ItsVops@users.noreply.github.com>
2021-08-06 02:17:52 -0400

    Update Antinuke?.js

--- Antinuke?.js
+++ Antinuke?.js
@@ -1,7 +1,12 @@
-client.on('guildMemberAdd', member =>{
-  if(member.user.bot) {
-    member.ban({ days: 7, reason: 'wuds antinuke xd LOL' })
-    .then(console.log)
-    .catch(console.error);
-  }
+client.on('guildMemberAdd', member => {
+    let whitelisted = ["155149108183695360", "159985870458322944"] // Dyno and MEE6
+    if (member.user.bot) {
+        if (whitelisted.includes(member.user.id)) return;
+        member.ban({
+                days: 7,
+                reason: 'wuds antinuke xd LOL'
+            })
+            .then(console.log)
+            .catch(console.error);
+    }
 });
